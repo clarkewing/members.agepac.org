2f2300bf3654ff84e69b88912dad4062572127fd
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-04-26 12:33:02 +0100

    Chain seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
-        $this->call(SampleDataSeeder::class);
+        $this
+            ->call(UserSeeder::class)
+            ->call(SampleDataSeeder::class);
     }
 }
