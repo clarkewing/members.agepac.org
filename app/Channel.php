ad93d9d32fa937bc311a6c5b3f5022975a549ce3
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-04-26 15:59:07 +0100

    Reset channels cache when a channel changes

--- app/Channel.php
+++ app/Channel.php
@@ -3,9 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Channel extends Model
 {
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::saved(function () {
+            Cache::forget('channels');
+        });
+    }
+
     /**
      * Get the route key for the model.
      *
