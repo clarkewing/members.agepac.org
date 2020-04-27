eca26f63e68ee5902a95211615fee659c25768aa
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-04-27 12:46:43 +0100

    Change seeder call to array form

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this
-            ->call(UserSeeder::class)
-            ->call(SampleDataSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            SampleDataSeeder::class
+        ]);
     }
 }
