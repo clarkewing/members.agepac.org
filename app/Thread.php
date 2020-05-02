a059633ffeee6d35a9d37d81a72615758c0f17bd
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-05-02 11:39:00 +0100

    Remove unnecessary appends property on Thread

--- app/Thread.php
+++ app/Thread.php
@@ -38,13 +38,6 @@ class Thread extends Model
     protected $withCount = ['replies'];
 
     /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
-    protected $appends = ['isSubscribedTo'];
-
-    /**
      * The attributes that should be cast to native types.
      *
      * @var array
