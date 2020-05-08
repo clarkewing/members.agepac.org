e7248a8b87953ce966fbee1f07a230436eeb83b4
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-05-08 10:45:48 +0100

    Prevent infinite loop

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -21,11 +21,13 @@ class SearchTest extends TestCase
         create(Thread::class, [], 2);
         create(Thread::class, ['body' => "A body with the {$search} term."], 2);
 
+        $maxTime = now()->addSeconds(20);
+
         do {
             sleep(.25);
 
-        } while (empty($results));
             $results = $this->getJson(route('threads.search') . "?query=$search")->json()['data'];
+        } while (empty($results) && now()->lessThan($maxTime));
 
         $this->assertCount(2, $results);
 
