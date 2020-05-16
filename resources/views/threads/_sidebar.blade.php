08754e1f7b25aab4b155e83a47614d127706f55a
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-05-16 10:01:06 +0100

    Hide button to create thread for unverified users

--- resources/views/threads/_sidebar.blade.php
+++ resources/views/threads/_sidebar.blade.php
@@ -1,7 +1,11 @@
-<a href="{{ route('threads.create', ['channel_id' => optional(Route::input('channel'))->id]) }}"
-   class="btn btn-block btn-success mb-3">
-    Nouvelle discussion
-</a>
+@if(Auth::user()->hasVerifiedEmail())
+    <a href="{{ route('threads.create', ['channel_id' => optional(Route::input('channel'))->id]) }}"
+       class="btn btn-block btn-success mb-3">
+        Nouvelle discussion
+    </a>
+@else
+    <p class="alert alert-warning text-center p-1">Confirme ton adresse e-mail pour participer</p>
+@endif
 
 @include('threads._search-bar')
 
