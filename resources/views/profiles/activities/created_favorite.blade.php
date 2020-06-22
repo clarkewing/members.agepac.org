f2dcf32f85878d875495f32aba2da9ece2a82eea
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-06-22 16:10:27 +0100

    Translate activity text

--- resources/views/profiles/activities/created_favorite.blade.php
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -1,6 +1,6 @@
 @component('profiles.activities.activity')
     @slot('heading')
-        {{ $profileUser->name }} favorited <a href="{{ $activity->subject->favoritable->path() }}">a post</a>.
+        {{ $profileUser->name }} a aimé <a href="{{ $activity->subject->favoritable->path() }}">un post</a>.
     @endslot
 
     @slot('body')
--- resources/views/profiles/activities/created_thread.blade.php
+++ resources/views/profiles/activities/created_thread.blade.php
@@ -1,6 +1,6 @@
 @component('profiles.activities.activity')
     @slot('heading')
-        {{ $profileUser->name }} published <a href="{{ $activity->subject->path() }}">{{ $activity->subject->title }}</a>
+        {{ $profileUser->name }} a publié <a href="{{ $activity->subject->path() }}">{{ $activity->subject->title }}</a>
     @endslot
 
     @slot('body')
