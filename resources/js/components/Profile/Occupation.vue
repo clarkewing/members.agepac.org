f2ff18dc4574172843c578f9dfe2eb7932f71d87
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-06-28 15:34:56 +0100

    Decouple form from Occupation component

--- resources/js/components/Profile/Occupation.vue
+++ resources/js/components/Profile/Occupation.vue
@@ -10,7 +10,7 @@
                 v-text="fields.status"
             ></h6>
 
-            <button class="btn btn-sm ml-2" data-toggle="modal" :data-target="'#' + uniqueId('editOccupation')">
+            <button class="btn btn-sm ml-2" data-toggle="modal" :data-target="'#' + 'editOccupation' + _uid">
                 <span class="sr-only">Modifier</span>
 
                 <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
@@ -22,7 +22,7 @@
                 </svg>
             </button>
 
-            <div ref="modal" class="modal fade" :id="uniqueId('editOccupation')" tabindex="-1" role="dialog">
+            <div ref="modal" class="modal fade" :id="'editOccupation' + _uid" tabindex="-1" role="dialog">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
                         <div class="modal-header bg-white">
@@ -39,166 +39,11 @@
                         </div>
 
                         <div class="modal-body">
-                            <form @submit.prevent="update" @keydown="form.onKeydown($event)">
-                                <div class="form-row">
-                                    <div class="form-group col-md-8">
-                                        <label :for="uniqueId('position')">Intitulé du poste</label>
-                                        <div class="input-group">
-                                            <input type="text"
-                                                   :id="uniqueId('position')"
-                                                   v-model="form.position"
-                                                   :class="['form-control', form.errors.has('position') ? 'is-invalid' : '' ]">
-
-                                            <div class="input-group-append">
-                                                <div class="input-group-text form-check form-check-inline mr-0">
-                                                    <input type="checkbox"
-                                                           :id="uniqueId('is_pilot')"
-                                                           v-model="isPilot"
-                                                           class="form-check-input">
-                                                    <label class="form-check-label" :for="uniqueId('is_pilot')">
-                                                        Emploi de pilote
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div v-if="form.errors.has('position')"
-                                             class="d-block invalid-feedback"
-                                             v-text="form.errors.get('position')">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group col-md-4">
-                                        <label :for="uniqueId('aircraft_id')">Avion exploité</label>
-                                        <select :id="uniqueId('aircraft_id')"
-                                                :disabled="! isPilot"
-                                                v-model="form.aircraft_id"
-                                                :class="['form-control', form.errors.has('aircraft_id') ? 'is-invalid' : '' ]">
-                                            <option value="" disabled></option>
-                                            <option v-for="aircraft of App.aircrafts"
-                                                    :value="aircraft.id"
-                                                    v-text="aircraft.name"
-                                            ></option>
-                                        </select>
-
-                                        <div v-if="form.errors.has('aircraft_id')"
-                                             class="invalid-feedback"
-                                             v-text="form.errors.get('aircraft_id')">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label :for="uniqueId('company')">Employeur</label>
-                                        <input type="text"
-                                               :id="uniqueId('company')"
-                                               v-model="form.company"
-                                               :class="['form-control', form.errors.has('company') ? 'is-invalid' : '' ]">
-
-                                        <div v-if="form.errors.has('company')"
-                                             class="invalid-feedback"
-                                             v-text="form.errors.get('company')">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group col-md-6">
-                                        <label :for="uniqueId('location')">Localisation</label>
-                                        <location-input :options="locationOptions"
-                                                        :id="uniqueId('location')"
-                                                        v-model="form.location"
-                                                        :class="{'is-invalid': form.errors.has('location')}"
-                                        ></location-input>
-
-                                        <div v-if="form.errors.has('location')"
-                                             class="d-block invalid-feedback"
-                                             v-text="form.errors.get('location')">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-row">
-                                    <div class="form-group col-sm-6 col-md">
-                                        <label :for="uniqueId('start_date')">Date de début</label>
-                                        <b-form-datepicker hide-header reset-button locale="fr" start-weekday="1"
-                                                           :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-                                                           :id="uniqueId('start_date')"
-                                                           v-model="form.start_date"
-                                                           :max="form.end_date"
-                                                           :class="[form.errors.has('start_date') ? 'is-invalid' : '' ]"
-                                        ></b-form-datepicker>
-
-                                        <div v-if="form.errors.has('start_date')"
-                                             class="invalid-feedback"
-                                             v-text="form.errors.get('start_date')">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group col-sm-6 col-md">
-                                        <label :for="uniqueId('end_date')">Date de fin</label>
-                                        <b-form-datepicker hide-header reset-button locale="fr" start-weekday="1"
-                                                           :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-                                                           :id="uniqueId('end_date')"
-                                                           v-model="form.end_date"
-                                                           :min="form.start_date"
-                                                           :max="new Date()"
-                                                           :class="[form.errors.has('end_date') ? 'is-invalid' : '' ]"
-                                        ></b-form-datepicker>
-
-                                        <div v-if="form.errors.has('end_date')"
-                                             class="invalid-feedback"
-                                             v-text="form.errors.get('end_date')">
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group col-md-5">
-                                        <label :for="uniqueId('status_code')">Statut</label>
-                                        <select :id="uniqueId('status_code')"
-                                                v-model="form.status_code"
-                                                :class="['form-control', form.errors.has('status_code') ? 'is-invalid' : '' ]">
-                                            <option value="" disabled></option>
-                                            <option v-for="(status, statusCode) of App.occupationStatuses"
-                                                    :value="statusCode"
-                                                    v-text="status"
-                                            ></option>
-                                        </select>
-
-                                        <div v-if="form.errors.has('status_code')"
-                                             class="invalid-feedback"
-                                             v-text="form.errors.get('status_code')">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label :for="uniqueId('escription')">Description</label>
-                                    <textarea :id="uniqueId('escription')"
-                                              rows="3"
-                                              v-model="form.description"
-                                              :class="['form-control', form.errors.has('description') ? 'is-invalid' : '' ]"
-                                    ></textarea>
-
-                                    <div v-if="form.errors.has('description')"
-                                         class="invalid-feedback"
-                                         v-text="form.errors.get('description')">
-                                    </div>
-                                </div>
-
-                                <div class="d-flex justify-content-end">
-                                    <button type="button" class="btn btn-outline-danger mr-auto"
-                                            @click="destroy">
-                                        Supprimer
-                                    </button>
-
-                                    <button type="button" class="btn btn-link mr-2" data-dismiss="modal">
-                                        Annuler
-                                    </button>
-
-                                    <button type="submit" class="btn btn-success">
-                                        Enregistrer
-                                    </button>
-                                </div>
-                            </form>
+                            <occupation-form v-bind:form.sync="form"
+                                             show-delete
+                                             @destroy="destroy"
+                                             @submit="update"
+                            ></occupation-form>
                         </div>
                     </div>
                 </div>
@@ -254,16 +99,14 @@
 </template>
 
 <script>
-    import {BFormDatepicker} from 'bootstrap-vue';
-    import 'bootstrap-vue/dist/bootstrap-vue.css';
-    import LocationInput from "../LocationInput";
     import {Form} from 'vform';
     import moment from 'moment';
+    import OccupationForm from "./OccupationForm";
     import updateProfile from "../../mixins/update-profile";
 
     export default {
         mixins: [updateProfile],
-        components: {BFormDatepicker, LocationInput},
+        components: {OccupationForm},
 
         props: [
             'id',
@@ -299,24 +142,6 @@
                 ])),
 
                 fields: _.omit(this.$props, []), // _.omit produces an independent copy
-
-                isPilot: this.is_pilot,
-
-                locationOptions: {
-                    templates: {
-                        value: (suggestion) => {
-                            if (suggestion.type === 'city' || suggestion.type === 'address') {
-                                return (suggestion.city || suggestion.name) + ', ' + suggestion.country;
-                            }
-
-                            if (suggestion.type === 'airport') {
-                                return suggestion.name + ', ' + suggestion.administrative + ', ' + suggestion.country;
-                            }
-
-                            return suggestion.name + ', ' + suggestion.city + ', ' + suggestion.administrative + ', ' + suggestion.country;
-                        }
-                    },
-                }
             };
         },
 
@@ -348,15 +173,6 @@
             }
         },
 
-        watch: {
-            isPilot(value) {
-                // Null aircraft_id if user unchecks isPilot.
-                if (!value) {
-                    this.form.aircraft_id = null;
-                }
-            }
-        },
-
         methods: {
             updated() {
                 $(this.$refs.modal).modal('hide');
@@ -369,10 +185,6 @@
                 flash('Emploi supprimé.');
 
                 this.$emit('destroyed', this.fields.id);
-            },
-
-            uniqueId(name) {
-                return name + this.fields.id;
             },
         },
 
