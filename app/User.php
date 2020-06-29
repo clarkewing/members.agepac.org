453cfd92f0d3101278da699dc01e7a50e53a0ead
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-06-29 22:33:48 +0100

    Cast flight hours to integer

--- app/User.php
+++ app/User.php
@@ -58,6 +58,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'flight_hours' => 'integer',
     ];
 
     /**
