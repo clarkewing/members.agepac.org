2e7606aba787154f548d69fbada74d1451a994ee
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-07-08 12:27:57 +0100

    Set unique constraint on slug

--- database/migrations/2020_07_04_082530_create_companies_table.php
+++ database/migrations/2020_07_04_082530_create_companies_table.php
@@ -15,7 +15,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->string('name');
             $table->unsignedTinyInteger('type_code');
             $table->string('website')->nullable();
