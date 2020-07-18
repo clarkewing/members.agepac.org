fb5301cdde92bc5885d3ae868b8a44ca89bcb90c
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-07-18 14:31:24 +0100

    Add NovaTestCase methods for other CRUD verbs

--- tests/NovaTestCase.php
+++ tests/NovaTestCase.php
@@ -2,11 +2,41 @@
 
 namespace Tests;
 
+use Laravel\Nova\Http\Controllers\ResourceDestroyController;
+use Laravel\Nova\Http\Controllers\ResourceIndexController;
+use Laravel\Nova\Http\Controllers\ResourceShowController;
 use Laravel\Nova\Http\Controllers\ResourceStoreController;
 use Laravel\Nova\Http\Controllers\ResourceUpdateController;
 
 abstract class NovaTestCase extends TestCase
 {
+    /**
+     * Send a GET request to index a Nova resource.
+     *
+     * @param  string  $resource
+     * @return \Illuminate\Testing\TestResponse
+     */
+    protected function indexResource(string $resource)
+    {
+        return $this->getJson(
+            action([ResourceIndexController::class, 'handle'], compact('resource'))
+        );
+    }
+
+    /**
+     * Send a GET request to view a Nova resource.
+     *
+     * @param  string  $resource
+     * @param  int|string  $resourceId
+     * @return \Illuminate\Testing\TestResponse
+     */
+    protected function showResource(string $resource, $resourceId)
+    {
+        return $this->getJson(
+            action([ResourceShowController::class, 'handle'], compact('resource', 'resourceId'))
+        );
+    }
+
     /**
      * Send a POST request to store a Nova resource.
      *
@@ -35,6 +65,23 @@ abstract class NovaTestCase extends TestCase
         return $this->putJson(
             action([ResourceUpdateController::class, 'handle'], compact('resource', 'resourceId')),
             $data
+        );
+    }
+
+    /**
+     * Send a DELETE request to delete a Nova resource.
+     *
+     * @param  string  $resource
+     * @param  int|string  $resourceId
+     * @return \Illuminate\Testing\TestResponse
+     */
+    protected function deleteResource(string $resource, $resourceId)
+    {
+        return $this->deleteJson(
+            action([ResourceDestroyController::class, 'handle'], [
+                'resource' => $resource,
+                'resources[]' => $resourceId,
+            ])
         );
     }
 }
