c1ffa898260e295600e5d13c6d8c314fc43441f4
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-07-19 16:03:21 +0100

    Clean up AuthServiceProvider with Policy Auto-Discovery
    Profile still needs to be explicitly mapped to UserPolicy, given the names don't match up.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,22 +2,9 @@
 
 namespace App\Providers;
 
-use App\Course;
 use App\Http\Middleware\Authenticate;
-use App\Occupation;
-use App\Page;
-use App\Policies\CoursePolicy;
-use App\Policies\OccupationPolicy;
-use App\Policies\PagePolicy;
-use App\Policies\PostPolicy;
-use App\Policies\ThreadPolicy;
-use App\Policies\UserInvitationPolicy;
 use App\Policies\UserPolicy;
-use App\Post;
 use App\Profile;
-use App\Thread;
-use App\User;
-use App\UserInvitation;
 use Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -25,19 +12,13 @@ use Illuminate\Support\Facades\Gate;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
+     * The explicit policy mappings for the application.
+     * All other policies are auto-discovered.
      *
      * @var array
      */
     protected $policies = [
-        Course::class => CoursePolicy::class,
-        Occupation::class => OccupationPolicy::class,
-        Thread::class => ThreadPolicy::class,
-        Page::class => PagePolicy::class,
-        Post::class => PostPolicy::class,
-        User::class => UserPolicy::class,
         Profile::class => UserPolicy::class,
-        UserInvitation::class => UserInvitationPolicy::class,
     ];
 
     /**
