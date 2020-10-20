18262036df9dcf9dafc4144b2f1a4e1c29f3c293
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-10-20 22:45:06 +0200

    Added migrations for the polling system
    Co-Authored-By: adraveny <63521496+adraveny@users.noreply.github.com>

---
+++ database/migrations/2020_10_20_204149_create_poll_votes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePollVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poll_votes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('option_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('option_id')
+                ->references('id')
+                ->on('poll_options')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poll_votes');
+    }
+}
