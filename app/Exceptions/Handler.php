ebe33b9103e0d1f551a97d468185fa095b07dd19
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-12-07 16:23:39 +0000

    Streamline the Exception Handler into a single register method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,31 +28,16 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \Throwable  $exception
      * @return void
-     * @throws \Exception
      */
-    public function report(Throwable $exception)
+    public function register()
     {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Throwable
-     */
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof ThrottleException) {
-            return Response::make('Du calme moussaillon. Tu postes beaucoup, prends une petite pause.', 429);
-        }
-
-        return parent::render($request, $exception);
+        $this->reportable(function (Throwable $e) {
+            if ($e instanceof ThrottleException) {
+                return Response::make('Du calme moussaillon. Tu postes beaucoup, prends une petite pause.', 429);
+            }
+        });
     }
 }
