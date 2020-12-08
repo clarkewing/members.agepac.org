f1c0cc741b6da2fd27f3eb6e7b8d1ca0339a4ae0
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-12-08 14:24:36 +0000

    Bring files in line with Laravel 8 defaults

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -44,7 +44,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         //
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,17 +8,6 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 class UserFactory extends Factory
 {
     /**
