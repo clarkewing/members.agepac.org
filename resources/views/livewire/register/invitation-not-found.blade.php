60fda3e83376408d4a56bd393b4848803d464da1
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-12-20 11:41:42 +0000

    Set links to starter pages

--- resources/views/livewire/register/invitation-not-found.blade.php
+++ resources/views/livewire/register/invitation-not-found.blade.php
@@ -30,7 +30,7 @@
             <span>Retour</span>
         </button>
 
-        <a class="btn btn-outline-info rounded-pill" href="/help">
+        <a class="btn btn-outline-info rounded-pill" href="{{ route('pages.show', 'help') }}">
             <svg class="bi bi-life-preserver mr-1" width="1em" height="1em" viewBox="0 0 16 16"
                  fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z"
