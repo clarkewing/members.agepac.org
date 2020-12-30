ab7e68f7a1da87895b27801fcc2e8e6f298043aa
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2020-12-30 10:33:46 +0000

    Add Thread resource to Nova (#66)
    * Add Thread resource to Nova
    
    * Add action to bulk update thread channels
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    Co-authored-by: clarkewing <clarkewing@users.noreply.github.com>

---
+++ app/Nova/Actions/UpdateThreadChannel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Models\Channel;
+use App\Models\Thread;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\Rule;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Select;
+
+class UpdateThreadChannel extends Action
+{
+    /**
+     * The displayable name of the action.
+     *
+     * @var string
+     */
+    public $name = 'Update Channel';
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        Thread::whereIn('id', $models->pluck('id'))
+            ->update(['channel_id' => $fields->channel_id]);
+
+        return Action::message('Channel successfully updated for selected threads.');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        $channels = Channel::withoutGlobalScopes()->get();
+
+        return [
+            Select::make('Channel', 'channel_id')
+                ->options($channels->pluck('name', 'id'))
+                ->rules(['required', Rule::in($channels->pluck('id'))]),
+        ];
+    }
+}
---
+++ tests/Feature/Nova/ManageThreadsTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Tests\Feature\Nova;
+
+use App\Models\Thread;
+use Tests\NovaTestRequests;
+use Tests\TestCase;
+
+class ManageThreadsTest extends TestCase
+{
+    use NovaTestRequests;
+
+    /** @test */
+    public function testUnauthorizedUsersCannotIndexThreads()
+    {
+        $this->signIn();
+
+        $this->indexResource('threads')
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function testUnauthorizedUsersCannotViewAThread()
+    {
+        $thread = Thread::factory()->create();
+
+        $this->signIn();
+
+        $this->showResource('threads', $thread->id)
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function testCreatingAThreadInNovaIsForbidden()
+    {
+        $this->signIn();
+
+        $this->storeResource('threads', Thread::factory()->make(['title' => 'Fake'])->toArray())
+            ->assertForbidden();
+
+        $this->signInWithPermission('threads.edit');
+
+        $this->storeResource('threads', Thread::factory()->make(['title' => 'Fake'])->toArray())
+            ->assertForbidden();
+
+        $this->assertDatabaseMissing('threads', ['title' => 'Fake']);
+    }
+
+    /** @test */
+    public function testUnauthorizedUsersCannotEditAThread()
+    {
+        $thread = Thread::factory()->create(['title' => 'Foobar']);
+
+        $this->signIn();
+
+        $this->updateThread(['title' => 'Fake title'], $thread)
+            ->assertForbidden();
+
+        $this->assertEquals('Foobar', $thread->fresh()->title);
+    }
+
+    /** @test */
+    public function testDeletingAThreadInNovaIsForbidden()
+    {
+        $thread = Thread::factory()->create();
+
+        $this->signIn();
+
+        $this->deleteResource('threads', $thread->id)
+            ->assertForbidden();
+
+        $this->signInWithPermission('threads.delete');
+
+        $this->deleteResource('threads', $thread->id)
+            ->assertForbidden();
+
+        $this->assertDatabaseHas('threads', ['id' => $thread->id]);
+    }
+
+    /** @test */
+    public function testAuthorizedUsersCanIndexThreads()
+    {
+        $this->signInWithPermission('threads.edit');
+
+        $this->indexResource('threads')
+            ->assertOk();
+    }
+
+    /** @test */
+    public function testAuthorizedUsersCanViewAThread()
+    {
+        $thread = Thread::factory()->create();
+
+        $this->signInWithPermission('threads.edit');
+
+        $this->showResource('threads', $thread->id)
+            ->assertOk();
+    }
+
+    /** @test */
+    public function testAuthorizedUsersCanEditAThread()
+    {
+        $thread = Thread::factory()->create();
+
+        $this->signInWithPermission('threads.edit');
+
+        $this->updateThread(['title' => 'Updated Thread'], $thread)
+            ->assertOk();
+
+        $this->assertDatabaseHas('threads', ['id' => $thread->id, 'title' => 'Updated Thread']);
+    }
+
+    /** @test */
+    public function testTitleIsRequired()
+    {
+        $this->signInWithPermission('threads.edit');
+
+        $this->updateThread(['title' => null])
+            ->assertJsonValidationErrors('title');
+    }
+
+    /**
+     * Submits a request to update an existing thread.
+     *
+     * @param  array  $data
+     * @param  \App\Models\Thread|null  $thread
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function updateThread(array $data = [], Thread $thread = null)
+    {
+        $thread = $thread ?? Thread::factory()->create();
+
+        return $this->updateResource(
+            'threads', $thread->id,
+            array_merge($thread->toArray(), $data)
+        );
+    }
+}
