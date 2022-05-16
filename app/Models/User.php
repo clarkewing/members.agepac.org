c7bdef7b7864c78dcd8708a1ae01ee5422bb24e0
Shift <shift@laravelshift.com>
2022-05-16 13:34:51 +0000

    Convert `optional()` to nullsafe operator

--- app/Models/User.php
+++ app/Models/User.php
@@ -300,7 +300,7 @@ class User extends Authenticatable implements MustVerifyEmail
             [
                 'name' => $this->name,
                 'description' => $this->class,
-                'phone' => optional($this->phone)->formatInternational(),
+                'phone' => $this->phone?->formatInternational(),
             ],
             $options
         ));
