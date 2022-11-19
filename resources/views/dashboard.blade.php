eec2cceb601e3fc8832e42601f26d93505397e35
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2022-11-19 21:48:05 +0100

    Feed WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,5 @@
 <x-app-layout :aside-on-top="false">
-    <div class="px-4 sm:px-0">
-        <x-tabs>
-            <x-tab active>Récent</x-tab>
-            <x-tab>Populaire</x-tab>
-            <x-tab>Suivis</x-tab>
-        </x-tabs>
-    </div>
-
-    <x-feed.recent class="mt-4" />
+    <livewire:feed />
 
     <x-slot name="aside">
         <div class="grid grid-cols-1 gap-6">
---
+++ resources/views/livewire/feed.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="px-4 sm:px-0">
+        <x-tabs>
+            <x-tab active>Récent</x-tab>
+            <x-tab>Populaire</x-tab>
+            <x-tab>Suivis</x-tab>
+        </x-tabs>
+    </div>
+
+    <x-feed.recent class="mt-4" />
+</div>
