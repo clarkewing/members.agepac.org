42789433b9ea456e2da177162a4db5514041c3bc
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2022-11-19 16:57:13 +0100

    Ensure UserFactory provides appropriate password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -25,7 +26,7 @@ class UserFactory extends Factory
             'username' => User::makeUsername($firstName, $lastName),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'class_course' => Arr::random(config('council.courses')),
             'class_year' => $this->faker->year(),
             'gender' => Arr::random(array_keys(config('council.genders'))),
