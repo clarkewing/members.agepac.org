709c0837beb00ac7e7bb0f9d9b8d2dde55fc5ad1
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2023-11-21 10:34:35 +0100

    Fix bug created by use of Nova v4 classes

--- app/Nova/Filters/ThreadStatus.php
+++ app/Nova/Filters/ThreadStatus.php
@@ -37,3 +37,43 @@ class ThreadStatus extends BooleanFilter
         ];
     }
 }
+
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\BooleanFilter;
+
+class ThreadStatus extends BooleanFilter
+{
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        // $value = ['pinned' => true / false, 'locked' => true / false]
+
+        return $query->where('pinned', $value['pinned'])
+                     ->where('locked', $value['locked']);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return [
+            'Pinned' => 'pinned',
+            'Locked' => 'locked',
+        ];
+    }
+}
