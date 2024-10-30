420359316385b229ba21e53eecd2a169ae4cc3a1
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2024-10-30 11:51:08 +0100

    Add handling for the case when an activity's subject is null

--- resources/views/profiles/activities/created_favorite.blade.php
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -1,9 +1,16 @@
 @component('profiles.activities.activity')
     @slot('heading')
-        {{ $profile->name }} a aimé <a href="{{ $activity->subject->favoritable->path() }}">un post</a>.
+        {{ $profile->name }} a aimé
+        @if(is_null($activity->subject))
+            un post qui n'existe plus.
+        @else
+            <a href="{{ $activity->subject->favoritable->path() }}">un post</a>.
+        @endif
     @endslot
 
-    @slot('body')
-        {{ $activity->subject->favoritable->body }}
-    @endslot
+    @unless(is_null($activity->subject))
+        @slot('body')
+            {{ $activity->subject->favoritable->body }}
+        @endslot
+    @endunless
 @endcomponent
