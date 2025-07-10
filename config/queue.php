9f96ee770e32525ec94950e7eb1dd5b255a0579e
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-07-10 15:44:14 +0200

    Remove Mailcoach config entries

--- config/queue.php
+++ config/queue.php
@@ -66,15 +66,6 @@ return [
             'retry_after' => 90,
             'block_for' => null,
         ],
-
-        'mailcoach-redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
-            'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => 11 * 60,
-            'block_for' => null,
-        ],
-
     ],
 
     /*
