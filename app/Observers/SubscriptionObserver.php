f91a6effae8ad02cb5b3961123cc13379fe2f840
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-07-10 15:45:12 +0200

    Remove Mailcoach email list subscription logic

--- app/Observers/SubscriptionObserver.php
+++ app/Observers/SubscriptionObserver.php
@@ -5,15 +5,10 @@ namespace App\Observers;
 use App\Actions\SubscribeUserToMembersNewsletterAction;
 use App\Actions\UnsubscribeUserFromMembersNewsletterAction;
 use App\Models\User;
-use App\Traits\ManagesEmailList;
 use Laravel\Cashier\Subscription as StripeSubscription;
 
 class SubscriptionObserver
 {
-    use ManagesEmailList;
-
-    protected string $emailListName = 'Members';
-
     /**
      * Handle the subscription "saved" event.
      *
@@ -22,19 +17,11 @@ class SubscriptionObserver
     public function saved(StripeSubscription $subscription)
     {
         if ($subscription->valid() || $this->hasActiveSubscription($subscription->user)) {
-            // Self-hosted Mailcoach
-            $this->addToEmailList($subscription->user);
-
-            // Mailcoach Cloud
             app(SubscribeUserToMembersNewsletterAction::class)->execute($subscription->user);
 
             return;
         }
 
-        // Self-hosted Mailcoach
-        $this->removeFromEmailList($subscription->user);
-
-        // Mailcoach Cloud
         app(UnsubscribeUserFromMembersNewsletterAction::class)->execute($subscription->user);
     }
 
@@ -46,16 +33,11 @@ class SubscriptionObserver
     public function deleted(StripeSubscription $subscription)
     {
         if ($this->hasActiveSubscription($subscription->user)) {
-            // Mailcoach Cloud
             app(SubscribeUserToMembersNewsletterAction::class)->execute($subscription->user);
 
             return;
         }
 
-        // Self-hosted Mailcoach
-        $this->removeFromEmailList($subscription->user);
-
-        // Mailcoach Cloud
         app(UnsubscribeUserFromMembersNewsletterAction::class)->execute($subscription->user);
     }
 
--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -6,40 +6,23 @@ use App\Actions\SubscribeUserToNewsletterAction;
 use App\Actions\UnsubscribeUserFromNewsletterAction;
 use App\Actions\UpdateUserNewsletterEmailAction;
 use App\Models\User;
-use App\Traits\ManagesEmailList;
 
 class UserObserver
 {
-    use ManagesEmailList;
-
-    protected string $emailListName = 'All Users';
-
     public function created(User $user)
     {
-        // Self-hosted Mailcoach
-        $this->addToEmailList($user);
-
-        // Mailcoach Cloud
         app(SubscribeUserToNewsletterAction::class)->execute($user);
     }
 
     public function updated(User $user)
     {
         if ($user->wasChanged('email')) {
-            // Self-hosted Mailcoach
-            $this->updateEmailListEmail($user->getOriginal('email'), $user->email);
-
-            // Mailcoach Cloud
             app(UpdateUserNewsletterEmailAction::class)->execute($user);
         }
     }
 
     public function deleted(User $user)
     {
-        // Self-hosted Mailcoach
-        $this->removeFromEmailList($user);
-
-        // Mailcoach Cloud
         app(UnsubscribeUserFromNewsletterAction::class)->execute($user);
     }
 }
