bffd4ca7c385eb778e8bf2ae3da02347504936a7
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-09-21 21:56:33 +0200

    Sync users to new database on creation

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -6,12 +6,16 @@ use App\Actions\SubscribeUserToNewsletterAction;
 use App\Actions\UnsubscribeUserFromNewsletterAction;
 use App\Actions\UpdateUserNewsletterEmailAction;
 use App\Models\User;
+use ClarkeWing\LegacySync\Enums\SyncDirection;
+use ClarkeWing\LegacySync\Facades\LegacySync;
 
 class UserObserver
 {
     public function created(User $user)
     {
         app(SubscribeUserToNewsletterAction::class)->execute($user);
+
+        LegacySync::syncRecord($user->getTable(), $user->getKey(), SyncDirection::LegacyToNew);
     }
 
     public function updated(User $user)
