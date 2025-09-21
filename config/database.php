6572cd1e1cb5b6e2cd4020e128901beeade7b50c
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-09-21 21:56:05 +0200

    Setup LegacySync

--- config/database.php
+++ config/database.php
@@ -91,6 +91,26 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'squawk' => [
+            'driver' => env('DB_SQUAWK_DRIVER'),
+            'url' => env('DB_SQUAWK_URL'),
+            'host' => env('DB_SQUAWK_HOST', '127.0.0.1'),
+            'port' => env('DB_SQUAWK_PORT', '3306'),
+            'database' => env('DB_SQUAWK_DATABASE', 'laravel'),
+            'username' => env('DB_SQUAWK_USERNAME', 'root'),
+            'password' => env('DB_SQUAWK_PASSWORD', ''),
+            'unix_socket' => env('DB_SQUAWK_SOCKET', ''),
+            'charset' => env('DB_SQUAWK_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_SQUAWK_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
@@ -123,7 +143,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
