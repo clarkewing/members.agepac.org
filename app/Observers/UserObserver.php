52f2d37cc9df3b0e88256c98a02bcb998e6b9c42
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-10-15 20:27:42 +0200

    Comment out Mailcoach actions in observers (#148)

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -13,20 +13,20 @@ class UserObserver
 {
     public function created(User $user)
     {
-        app(SubscribeUserToNewsletterAction::class)->execute($user);
+        // app(SubscribeUserToNewsletterAction::class)->execute($user);
 
         LegacySync::syncRecord($user->getTable(), $user->getKey(), SyncDirection::LegacyToNew);
     }
 
     public function updated(User $user)
     {
-        if ($user->wasChanged('email')) {
-            app(UpdateUserNewsletterEmailAction::class)->execute($user);
-        }
+        // if ($user->wasChanged('email')) {
+        //     app(UpdateUserNewsletterEmailAction::class)->execute($user);
+        // }
     }
 
     public function deleted(User $user)
     {
-        app(UnsubscribeUserFromNewsletterAction::class)->execute($user);
+        // app(UnsubscribeUserFromNewsletterAction::class)->execute($user);
     }
 }
